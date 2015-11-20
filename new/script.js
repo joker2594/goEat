6797b8b5bc771127f0ead013f6fb986fcb96217f
2092020g <2092020g@student.gla.ac.uk>
2015-11-20 12:17:47 +0000

    added new index page branch

---
+++ new/script.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+  var sidebar = false;
+  var cuisines = ['Chinese', 'Japanese', 'Italian', 'Greek', 'American', 'Indian', 'African'];
+  var searches = ['McDonalds', 'Chinese near West End', 'City centre'];
+
+  $(window).load(function() {
+    for (i = 0; i < cuisines.length; i++) {
+      $('#cats').append("<div class=\'filter\' cuisineitem>" + cuisines[i] + "</div> ");
+    }
+  });
+
+  $(window).load(function() {
+    for (i = 0; i < searches.length; i++) {
+      $('#searchhistory').append("<div class=\'filter\' searchitem>" + searches[i] + "</div> ");
+    }
+  });
+
+  $('#sidebaricon').hover(function() {
+    $(this).css("background-image", "url('images/sidebarselected.png')");
+  },
+  function() {
+    $(this).css("background-image", "url('images/sidebar.png')");
+  });
+
+  $(document).on("mouseenter", ".filter", function() {
+    $(this).css("background-color", "#f19132");
+    $(this).css("color", "#ffffff");
+  });
+
+  $(document).on("mouseout", ".filter", function() {
+    $(this).css("background-color", "#ffffff");
+    $(this).css("color", "#000000");
+  });
+
+  $('#sidebaricon').click(function() {
+    if (sidebar) {
+      $('#sidebar').css("display", "none");
+      sidebar = false;
+    }
+    else {
+      $('#sidebar').css("display", "table");
+      sidebar = true;
+    }
+  });
+});
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 55.86515, lng: -4.25763},
+    zoom: 14
+  });
+}
